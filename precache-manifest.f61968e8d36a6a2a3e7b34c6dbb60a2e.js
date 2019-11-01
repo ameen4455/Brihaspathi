660aed34bd4253815b8d5e7a4df8df273b92a932
AFNAN NAVAZ <afnan@pop-os.localdomain>
2019-11-01 23:46:02 +0530

    Updates

---
+++ precache-manifest.f61968e8d36a6a2a3e7b34c6dbb60a2e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a0063636f43df07680c",
+    "url": "/Brihaspathi/static/js/4.7a006363.chunk.js"
+  },
+  {
+    "revision": "aff9ae5e6ac1fc5b4ce0",
+    "url": "/Brihaspathi/static/css/main.c60a3a6f.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Brihaspathi/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fb51f2e1d5939059fbc4",
+    "url": "/Brihaspathi/static/js/1.fb51f2e1.chunk.js"
+  },
+  {
+    "revision": "00b0693ecc25f1b0e8f1",
+    "url": "/Brihaspathi/static/js/2.00b0693e.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Brihaspathi/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "d9585313207f968755c3",
+    "url": "/Brihaspathi/static/js/3.d9585313.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Brihaspathi/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "aff9ae5e6ac1fc5b4ce0",
+    "url": "/Brihaspathi/static/js/main.aff9ae5e.chunk.js"
+  },
+  {
+    "revision": "38c39d75ebec94f68cae",
+    "url": "/Brihaspathi/static/js/runtime~main.38c39d75.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Brihaspathi/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Brihaspathi/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7a0063636f43df07680c",
+    "url": "/Brihaspathi/static/css/4.e67b9e56.chunk.css"
+  },
+  {
+    "revision": "d9585313207f968755c3",
+    "url": "/Brihaspathi/static/css/3.5d18597b.chunk.css"
+  },
+  {
+    "revision": "fb51f2e1d5939059fbc4",
+    "url": "/Brihaspathi/static/css/1.a8756a8e.chunk.css"
+  },
+  {
+    "revision": "20e2197a7fab0700edb7af530b905161",
+    "url": "/Brihaspathi/index.html"
+  }
+];
